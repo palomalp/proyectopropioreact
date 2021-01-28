e5e0754f670eec7e4f46abb650c2bef5be91ed00
PalomaLP <palomaledesmap@gmail.com>
2021-01-28 15:34:37 +0100

    segunda parte del tutorial

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) { //el constructor crea los objetos de la clase.
+    super(props) //forma estándar de llamar al constructor
+    this.state = {
+      newTask: "",
+    }
+  }
+
+//esta es la función que luego le paso a onChange. Aquí se define la función y abajo en el input se llama.
+  handleTaskChange = (event) => {
+    this.setState ({newTask: event.target.value, //this.setState es para poder cambiar los estados que son inmutables.
+    })
+  }
+
+render() {
   return (
     <div className="container">
       <h1 className="title">
         Hello world <span aria-label="emoji" role="img">🔥</span>
       </h1>
-      <input type="text" className="new-task"/>
+      <input onChange={this.handleTaskChange} type = "text" className= "new-task"/> {/* //a onChange se le asigna una funcion que será llamada cada vez que cambie algo en el input */}
+  {/* //this.handleTaskChange es la función que se va a ejecutar cada vez que cambiemos el contenido del input */}
+      <h2 className = "test-label"> {this.state.newTask}</h2> 
     </div>
     );
+  }
 }
 
 export default App;
